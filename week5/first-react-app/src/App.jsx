cf0dcbe756d4d5d7920fa32233d88b4383023afc
Ankit Rath <ankitrath62@gmail.com>
2024-06-13 14:09:37 +0530

    first-react-app & todo app

--- week5/first-react-app/src/App.jsx
+++ week5/first-react-app/src/App.jsx
@@ -2,19 +2,46 @@ import { useState } from 'react'
 import './App.css'
 
 
-
-
 function App() {
+  const dlist = [{
+    title: "Welcome to the first react app",
+    description: "This is being printed using an iteration",
+    completed: true
+  },
+  {
+    title: "second",
+    description: "third",
+    completed: false
+  }
+]
+  const [list, setList] = useState(dlist)
   const [count, setCount] = useState(0)
   
-  
-  
-  
+  function addTodo(){
+    setList([...list, {
+      title: "Third",
+      description: "This was done using the spread command. \"...list\" means adding all elements of list",
+      completed: false
+    }])
+  }
+
   return (
     <div>
-      <CustomButton count={count} setCount={setCount}></CustomButton>
+      <button onClick={addTodo}>Add Random Todo</button>
+      {list.map((todo)=>{
+      return <Todo title={todo.title} description={todo.description}></Todo>
+    })}
+      
     </div>
+    
   )
+}
+
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h4>{props.description}</h4>
+  </div>
 }
 
 //btn component
