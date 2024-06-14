55eda73cf2ae1496e5361e9a8baebebb1f2c3c17
Ankit Rath <ankitrath62@gmail.com>
2024-06-14 12:00:25 +0530

    shifted get todo call to Home comp: Reduces re-rendering

--- week5/full-stack-todo-app/frontend/src/App.jsx
+++ week5/full-stack-todo-app/frontend/src/App.jsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
 import { Home } from './pages/Home'
 
 function App() {
-  const [todos, setTodos] = useState([])
-  useEffect(()=> {
-    let list = async()=>{
-      const response = await fetch("http://localhost:4000/todos")
-      const json = await response.json()
-      //console.log(json)
-      setTodos(json)
-    }
-    list()
-    }, [])
-  
-  
-  
 
   return (
     <div>
       <Header />
-      <Home todos={todos}/>
+      <Home/>
     </div>
     
   )
--- week5/full-stack-todo-app/frontend/src/pages/Home.jsx
+++ week5/full-stack-todo-app/frontend/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import { CreateTodo } from "../components/CreateTodo";
 import { TodoList } from "../components/Todolist";
+import { useEffect, useState } from 'react'
 import './home.css'
-export function Home(props){
+
+
+export function Home(){
+    const [todos, setTodos] = useState([])
+    useEffect(()=> {
+    
+    let list = async()=>{
+      const response = await fetch("http://localhost:4000/todos")
+      const json = await response.json()
+      //console.log(json)
+      setTodos(json)
+    }
+    list()
+    }, [])
+
     return(
         <div className="container-body">
             <CreateTodo/>
-            <TodoList todos={props.todos}/>
+            <TodoList todos={todos}/>
         </div>
     )
 }
