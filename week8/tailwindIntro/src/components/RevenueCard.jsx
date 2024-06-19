d0b4b1217b06ffd39e34dfd691d48b468a8c356c
Ankit Rath <ankitrath62@gmail.com>
2024-06-19 22:04:37 +0530

    tailwind - sidebar and cards

---
+++ week8/tailwindIntro/src/components/RevenueCard.jsx
@@ -0,0 +1,31 @@
+
+export const RevenueCard = ({title, infoMsg, orderCount, amount})=>{
+
+    return <div className="bg-white rounded-lg shadow-md p-4">
+        <div className="flex items-center">
+        <div className="text-gray-700 align-middle">
+        {title}
+        </div>
+        <div className="ml-1 fill-grey-400">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="size-4">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0ZM8.94 6.94a.75.75 0 1 1-1.061-1.061 3 3 0 1 1 2.871 5.026v.345a.75.75 0 0 1-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 1 0 8.94 6.94ZM10 15a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        
+            
+        </div>
+        <div className="mt-2 md:flex justify-between items-center">
+            <div className="text-lg font-bold text-cyan-400">
+                ${amount}
+            </div>
+            <div>
+                {orderCount ? <div className="flex text-sm text-cyan-500">
+                    {orderCount} order(s) <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+  <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+</svg>
+
+                </div> : null}
+            </div>
+        </div>
+    </div>
+}
