f6e5bbd2ef78aaaa798fcd109e36807122a21862
Ankit Rath <ankitrath62@gmail.com>
2024-06-21 21:23:26 +0530

    minor changes

--- week8/paytm/frontend/src/components/Comp_SubHeading.jsx
+++ week8/paytm/frontend/src/components/Comp_SubHeading.jsx
@@ -1,6 +1,6 @@
 export function SubHeading({label}){
 
-    return <div className="font-regular text-sm pt-6 text-gray-700">
+    return <div className="font-regular text-sm pt-2 text-gray-700">
         {label}
     </div>
 
--- week8/paytm/frontend/src/pages/Signup.jsx
+++ week8/paytm/frontend/src/pages/Signup.jsx
@@ -3,21 +3,26 @@ import { Button } from "../components/Comp_Button";
 import { Heading } from "../components/Comp_Heading";
 import { InputBox } from "../components/Comp_InputBox";
 import { SubHeading } from "../components/Comp_SubHeading";
+//bg-gradient-to-r from-slate-900 to-slate-700
 
 export function Signup(){
-    return <div className="bg-slate-300 h-screen flex justify-center">
-        <div className="flex flex-col justify-center">
-            <div className="rounded-lg bg-white w-90 text-center p-2 h-max px-4">
-                <Heading label="Welcome"/>
-                <SubHeading label="Let's get started! Create your account by filling these details!"/>
-                <InputBox label="First Name" placeholder="Ankit"/>
-                <InputBox label="Last Name" placeholder="Rath"/>
-                <InputBox label="Email" placeholder="ankit@abc.com"/>
-                <InputBox label="Password" placeholder="******"/>
-                <Button label="Create Account"/>
-                <BottomWarning label="Already have an account?" buttonText="Log In" to="/Login"/>
+    return <div className="bg-[url('../assets/herobg.jpg')] h-screen flex"> 
+        <div className="flex flex-col px-4">
+        <div className="rounded-lg bg-white w-90 text-center p-4 h-full px-8 my-4 flex items-center justify-center">
+        <div className="w-full">
+        <div className="text-left">
+          <Heading label="Welcome to AkroPay"/>
+          <SubHeading label="Let's get started! Create your account by filling these details!"/>
+        </div>
+        <InputBox label="First Name" placeholder="Ankit"/>
+        <InputBox label="Last Name" placeholder="Rath"/>
+        <InputBox label="Email" placeholder="ankit@abc.com"/>
+        <InputBox label="Password" placeholder="******"/>
+        <Button label="Create Account"/>
+        <BottomWarning label="Already have an account?" buttonText="Log In" to="/Login"/>
+      </div>
+    </div>
+                
             </div>
         </div>
-        
-    </div>
 }
